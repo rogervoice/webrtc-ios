deb532d05f9c18df5a289f8ca5082dbe787ea436
Julien <julien.colin@rogervoice.com>
2025-07-04 11:08:54 +0200

    Move framework from sources to release

--- Package.swift
+++ Package.swift
@@ -1,6 +1,9 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+let version = "2.0.0"
+let checksum = "sha256:71ff62ae921d37aa1a5a8cb14bd2dd12ad47e2cfd15dc42f8aa1511829b1c6a0"
+
 let package = Package(
     name: "WebRTC",
     platforms: [
@@ -16,6 +19,11 @@ let package = Package(
         .binaryTarget(
             name: "WebRTC",
             path: "./WebRTC.xcframework"
-        ),
+        ),,
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/Toldy/webrtc-ios/releases/download/\(version)/WebRTC.xcframework.zip",
+            checksum: checksum
+        )
     ]
 )
